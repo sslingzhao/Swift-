41784727d8b317e4bdbc35924a19d5c9d855bb3f
ShineLing <987410331@qq.com>
2017-08-11 12:48:29 +0800

    创建Swift练习Demo

---
+++ SLZhao_Swift/SLZhao_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SLZhao_Swift
+//
+//  Created by mac on 2017/8/11.
+//  Copyright © 2017年 ShineLing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
